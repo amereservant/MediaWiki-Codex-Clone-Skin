96c09381ba37340ca02845ea3b3f840b59a047b4
David Miles <amereservant@gmail.com>
2011-06-03 01:33:07 -0500

    First commit.

---
+++ Codex.php
@@ -0,0 +1,341 @@
+<?php
+/**
+ * Mediawiki Codex Skin
+ *
+ * This is a clone of the {@link http://codex.wordpress.org} theme and licensed under
+ * the GPL License.
+ *
+ * All credit is due to the designers of {@link http://codex.wordpress.org} since this
+ * is a copy of their work and I just cloned their design and made it into a Mediawiki skin
+ * for personal use.
+ * 
+ * The best I can tell is that the design is part of the content of the WordPress.org Codex
+ * and falls under their GPL licensing of that site's contents, so that same licensing
+ * applies to this skin as well.
+ *
+ * @license     http://wordpress.org/about/gpl/ GPL
+ */
+
+if( !defined('MEDIAWIKI') ) die( -1 );
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ * @todo document
+ * @ingroup Skins
+ */
+class SkinCodex extends SkinTemplate
+{
+	/** Using codex. */
+	var $skinname  = 'codex';
+	var $stylename = 'codex';
+	var $template  = 'CodexTemplate';
+	var $useHeadElement = true;
+    
+    
+	function setupSkinUserCss( OutputPage $out )
+	{
+		global $wgHandheldStyle;
+
+		parent::setupSkinUserCss( $out );
+
+		// Append to the default screen common & print styles...
+		$out->addStyle( 'codex/main.css', 'screen' );
+		$out->addStyle( 'codex/wp4.css', 'screen' );
+		$out->addStyle( 'codex/iphone.css', 'only screen and (max-device-width: 480px)' );
+	}
+}
+
+/**
+ * Codex Template Class
+ *
+ * This class assembles the components of the Codex template and renders the output.
+ * It is written in <b>PHP5 OO Standards</b>, therefore it's not compatible with PHP4.
+ *
+ * @ingroup Skins 
+ */
+class CodexTemplate extends QuickTemplate
+{
+   /**
+    * Sidebox Group printf Format
+    *
+    * @var      string
+    * @access   private
+    */
+	private $_sideboxf = <<<________EOD
+	    <h3>%s</h3>
+	    <ul class="submenu">
+	        %s
+	    </ul>
+________EOD;
+
+   /**
+    * Sidebox List Item printf Format
+    *
+    * @var      string
+    * @access   private
+    */
+    private $_lif = "\t<li id=\"%s\"%s>%s</li>\n";
+
+   /**
+    * Sidebox AnchorLink printf Format
+    *
+    * @var      string
+    * @access   private
+    */
+    private $_af = '<a href="%s" %s>%s</a>';
+    
+   /**
+    * The Skin Object
+    *
+    * @var      object
+    * @access   public
+    */
+	public $skin;
+	
+	/**
+	 * Template Filter Callback
+	 *
+	 * Takes an associative array of data set from a SkinTemplate-based class, and a 
+	 * wrapper for MediaWiki's localization database, and outputs a formatted page.
+	 *
+	 * The page's HTML layout is included by calling the file {@link body.php}, which
+	 * is the primary theme file.
+	 *
+	 * @param   void
+	 * return   string  Outputs the pages generated content
+	 * @access  public
+	 */
+	public function execute()
+	{
+		global $wgRequest;
+        
+		$this->skin = $skin = $this->data['skin'];
+		$action     = $wgRequest->getText( 'action' );
+
+		// Suppress warnings to prevent notices about missing indexes in $this->data
+		wfSuppressWarnings();
+        
+        // Load the head element for the page
+		$this->html( 'headelement' );
+		
+		// Include the main content syntax
+		require realpath(dirname(__FILE__) .'/codex/body.php');
+	    
+	    // Restore warnings
+	    wfRestoreWarnings();
+	}
+
+   /**
+	* Generate Search Box
+	*
+	* Creates the search form for the Codex theme.
+	*
+	* @param    void
+	* @return   string  Prints the search form.
+	* @access   public
+	*/
+	public function searchBox() 
+	{
+		$format  = '<form action="%s" id="head-search">'."\n\t%s\n\t";
+		$format .= '<input type="submit" name="go" class="button" id="searchGoButton" value="Go"%s />'."\n";
+		$format .= "</form>\n";
+		
+		printf( $format,
+		    htmlspecialchars($this->data['wgScript']),
+            Html::input( 'text',
+			    isset($this->data['search']) && strlen($this->data['search']) > 0 ? $this->data['search'] : 'Search the Codex', 
+			    'text',
+			    array(
+				    'maxlength' => 150,
+				    'class'     => 'text',
+				    'title'     => $this->skin->titleAttrib( 'search' ),
+				    'accesskey' => $this->skin->accesskey( 'search' ),
+				    'onfocus'   => "this.value=(this.value=='Search the Codex') ? '' : this.value;",
+				    'onblur'    => "this.value=(this.value=='') ? 'Search the Codex' : this.value;",
+			    )
+            ),
+            $this->skin->tooltipAndAccesskey( 'search-go' )
+        );
+	}
+
+   /**
+    * ToolBox Sidebox
+    *
+    * Formats and prints the HTML syntax for the ToolBox links.
+    *
+    * @param    void
+    * @return   string  Prints the HTML syntax that makes up the ToolBox links and section.
+    * @access   public
+	*/
+	public function toolBox()
+	{
+	    $title = $this->translator->translate('Toolbox');
+	    $li    = '';
+	    
+        if( $this->data['notspecialpage'] )
+            $li .= sprintf($this->_lif, 
+			    't-whatlinkshere', 
+			    '', 
+			    sprintf( $this->_af, htmlspecialchars($this->data['nav_urls']['whatlinkshere']['href']), 
+			             $this->skin->tooltipAndAccesskey('t-whatlinkshere'), 
+			             htmlspecialchars( $this->translator->translate('whatlinkshere') ) )
+			);
+		
+		if( $this->data['feeds'] )
+	    {
+		    $alinks .= '';
+		    foreach($this->data['feeds'] as $key => $feed)
+		    {
+			    $alinks .= sprintf( $this->_af, 
+			        htmlspecialchars($feed['href']),
+			        'id="'. Sanitizer::escapeId( "feed-$key" ) .'" rel="alternate" type="application/'. $key .'+xml" class="feedlink" '.
+			        $this->skin->tooltipAndAccesskey('feed-'.$key),
+			        htmlspecialchars($feed['text']) .'&nbsp' );
+		    }
+		    
+	        $li .= sprintf($this->_li_lif, $this->msg('feedlinks', TRUE), '', $alinks);
+	    }
+
+	    foreach( array('recentchangeslinked', 'trackbacklink', 'contributions', 'log', 'blockip', 'emailuser', 'upload', 'specialpages') as $special )
+	    {
+		    if( is_array($this->data['nav_urls'][$special]) )
+		        $li .= sprintf( $this->_lif,
+		            't-'. $special,
+		            '',
+		            sprintf( $this->_af,
+		                htmlspecialchars($this->data['nav_urls'][$special]['href']),
+		                $this->skin->tooltipAndAccesskey('t-'. $special),
+		                htmlspecialchars( $this->translator->translate($special) ) )
+		        );
+	    }
+
+	    if( strlen($this->data['nav_urls']['print']['href']) > 0 )
+		    $li .= sprintf( $this->_lif,
+		        't-print',
+		        '',
+		        sprintf( $this->_af,
+		            htmlspecialchars($this->data['nav_urls']['print']['href']),
+			        'rel="alternate" '. $this->skin->tooltipAndAccesskey('t-print'),
+			        htmlspecialchars( $this->translator->translate('printableversion') ) )
+		    );
+	    
+	    if( strlen($this->data['nav_urls']['permalink']['href']) > 0 )
+	        $li .= sprintf( $this->_lif,
+			    't-permalink',
+			    '',
+			    sprintf( $this->_af,
+			        htmlspecialchars($this->data['nav_urls']['permalink']['href']),
+			        $this->skin->tooltipAndAccesskey('t-permalink'),
+			        htmlspecialchars( $this->translator->translate('permalink') ) )
+			);
+	    
+	    else
+			$li .= sprintf( $this->_lif,
+			    't-ispermalink',
+			    $this->skin->tooltip('t-ispermalink'),
+			    htmlspecialchars( $this->translator->translate('permalink') ) 
+			);
+	    
+	    printf($this->_sideboxf, $title, $li);
+        wfRunHooks( 'CodexTemplateToolboxEnd', array( &$this ) );
+	    wfRunHooks( 'SkinTemplateToolboxEnd', array( &$this ) );
+    }
+   
+   /**
+    * Views Sidebox
+    *
+    * This method formats and prints the <i>Views</i> Sidebox menu items.
+    *
+    * @param    void
+    * @return   string  Prints the formatted HTML syntax for the Views sidebox section.
+    * @access   public
+    */
+    public function viewsBox()
+    {
+        $title = $this->translator->translate('Views');
+    	$li    = '';
+    	
+    	foreach($this->data['content_actions'] as $key => $tab)
+		{
+			$id     = Sanitizer::escapeId("ca-{$key}");
+			$class  = $tab['class'] ? ' class="'. htmlspecialchars($tab['class']) .'"' : '';
+			$href   = htmlspecialchars($tab['href']);
+			$tool   = in_array( $action, array('edit', 'submit') ) && 
+			          in_array( $key, array('edit', 'watch', 'unwatch') ) ? 
+			          $this->skin->tooltip( "ca-$key" ) : $this->skin->tooltipAndAccesskey( "ca-$key" );
+			$text   = htmlspecialchars($tab['text']);
+			
+			$alink  = sprintf($this->_af, $href, $tool, $text);
+			$li     .= sprintf($this->_lif, $id, $class, $alink);
+	    }
+		
+		printf($this->_sideboxf, $title, $li);
+	}
+
+	/*************************************************************************************************/
+	public function languageBox()
+	{
+	    if( !$this->data['language_urls'] ) return;
+        
+        $links = '';
+        foreach($this->data['language_urls'] as $langlink)
+        { 
+				$links .= sprintf( $this->_lif,
+				    'lang-'. htmlspecialchars($langlink['text']),
+				    ' class="'. htmlspecialchars($langlink['class']) .'"',
+				    sprintf( $this->_af,
+				        htmlspecialchars($langlink['href']),
+				        '',
+				        htmlspecialchars( $this->translator->translate($langlink['text']) )
+				    )
+				);
+        }
+        
+        printf( $this->_sideboxf, $this->html('userlangattributes'), htmlspecialchars( $this->translator->translate('otherlanguages') ), $links );
+	}
+
+   /**
+    * Create Custom Sidebox
+    *
+    * This is used to add a custom sidebox section.
+    * 
+    * @param    string          $bar    Unsure
+    * @param    array|string    $cont   The content to add to the Sidebox.  It can be
+    *                                   an array of items to itterate over or an already
+    *                                   processed string of data to add directly.
+    * @return   string                  Prints out the formatted Sidebox syntax.
+    * @access   public
+    * @todo     Try making this method serve the other Sidebox methods in this class
+    *           by processing the data for them and minimizing the code in them if possible.
+	*/
+	public function customBox( $bar, $cont )
+	{
+        $links = '';
+		$out   = wfMsg( $bar );
+		$title = wfEmptyMsg($bar, $out) ? htmlspecialchars( $this->translator->translate($bar) ) : 
+		    htmlspecialchars( $this->translator->translate($out) );
+		
+		if ( !is_array($cont) ) 
+		{
+		    printf( $this->_sideboxf, $title, $cont );
+		    return;
+		}
+		
+		foreach($cont as $key => $val)
+		{ 
+				$links .= sprintf( $this->_lif,
+				    Sanitizer::escapeId($val['id']),
+				    ($val['active'] ? ' class="active"':''),
+				    sprintf( $this->_af,
+				        htmlspecialchars($val['href']),
+				        $this->skin->tooltipAndAccesskey($val['id']),
+				        htmlspecialchars( $this->translator->translate($val['text']) )
+				    )
+				);
+		}
+        
+        printf($this->_sideboxf, $title, $links);
+	}
+} // end of class
+
+
---
+++ codex/body.php
@@ -0,0 +1,119 @@
+<div id="header">
+	<div class="wrapper">
+	    <h1><a href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href']); ?>" title="<?php $this->html('title'); ?>" /><?php $this->html('title'); ?></a></h1>
+	    <?php $this->searchBox(); ?>
+        <ul>
+			<li><a href="#" title="">Home</a></li>
+			<li><a href="#" title="">Showcase</a></li>
+			<li><a href="#" title="">Extend</a>
+                <ul class="nav-submenu">
+				    <li><a href="#" title="">Plugins</a></li>
+				    <li><a href="#" title="">Themes</a></li>
+			    </ul>
+			</li>
+			<li><a href="#" title="">About</a></li>
+			<li><a class="current" href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href']); ?>" title="Documentation, tutorials, best practices.">Docs</a></li>
+			<li><a href="#" title="">Blog</a></li>
+
+			<li><a href="#" title="">Forums</a></li>
+			<li><a href="#" title="">Hosting</a></li>
+			<li id="download"><a href="#" title="Get it. Got it? Good.">Download</a></li>
+		</ul>
+	</div>
+</div><!-- #header -->
+<div id="headline">
+    <div class="wrapper">
+        <h2>Codex</h2>
+        <div class="portlet" id="p-personal">
+		    <p class="login">Codex tools:
+		    <?php foreach($this->data['personal_urls'] as $key => $item) {
+		        $linkf = '<a href="%s" class="%s" %s>%s</a>';
+		        $class = $item['class'] .' '. ($item['active'] ? 'active':'');
+		        printf( $linkf, htmlspecialchars($item['href']), $class, $skin->tooltipAndAccesskey('pt-'.$key), htmlspecialchars($item['text']) );
+		    } ?>
+		    </p>
+	    </div>
+	</div>
+</div><!-- #headline -->
+
+<div id="pagebody" <?php $this->html("specialpageattributes"); ?>>
+	<div class="wrapper">
+	<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice'); ?></div><?php } ?>
+        <div id="bodyContent" class="col-10">
+		    <h2 class="pagetitle"><?php $this->html('title'); ?></h2>
+		    <!-- start content -->
+		    <?php $this->html('bodytext'); ?>
+		    <?php if($this->data['catlinks']) { $this->html('catlinks'); } ?>
+		    <!-- end content -->
+		    <?php if($this->data['dataAfterContent']) { $this->html('dataAfterContent'); } ?>
+	    </div><!-- #bodyContent -->
+	    <div class="col-2">
+	        <?php $this->viewsBox(); ?>
+            <?php $this->toolBox(); ?>
+            <?php $this->languageBox(); ?>
+	    </div>
+    </div>
+</div><!-- #pagebody -->
+<?php
+/*
+		$sidebar = $this->data['sidebar'];
+		if ( !isset( $sidebar['TOOLBOX'] ) ) $sidebar['TOOLBOX'] = true;
+		if ( !isset( $sidebar['LANGUAGES'] ) ) $sidebar['LANGUAGES'] = true;
+		foreach ($sidebar as $boxName => $cont) {
+			if ( $boxName == 'TOOLBOX' ) {
+				$this->toolbox();
+			} elseif ( $boxName == 'LANGUAGES' ) {
+				$this->languageBox();
+			} else {
+				$this->customBox( $boxName, $cont );
+			}
+		}
+*/
+?>
+
+<div id="footer"<?php $this->html('userlangattributes') ?>>
+    <div class="wrapper">
+<?php 
+        // Generate additional footer links
+		$footerlinks = array(
+			'lastmod', 'viewcount', 'numberofwatchingusers', 'credits', 'copyright',
+			'privacy', 'about', 'disclaimer', 'tagline',
+		);
+		$validFooterLinks = array();
+		
+		foreach( $footerlinks as $aLink ) {
+			if( isset( $this->data[$aLink] ) && $this->data[$aLink] ) {
+				$validFooterLinks[] = $aLink;
+			}
+		}
+		if ( count( $validFooterLinks ) > 0 ) { ?>
+		<p>
+        <?php
+        $i = 0;
+        $c = count($validFooterLinks);
+		foreach( $validFooterLinks as $aLink )
+		{
+		    if( isset( $this->data[$aLink] ) && $this->data[$aLink] )
+		    {
+                $this->html($aLink);
+                echo $i < $c ? ' | ':'';
+            }
+            $i++;
+        }
+?>      </p>
+<?php   } ?>
+        <h6>Code is Poetry</h6>
+    </div>
+</div><!-- #footer -->
+<?php
+$this->html('bottomscripts'); /* JS call to runBodyOnloadHook */
+$this->html('reporttime');
+
+if( $this->data['debug'] ): ?>
+<!-- Debug output:
+<?php $this->text( 'debug' ); ?>
+
+-->
+<?php endif; ?>
+</body>
+</html>
